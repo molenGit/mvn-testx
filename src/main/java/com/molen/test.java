4e4cceaf857ee04eff329f8b4643a3cdc9501d0a
molen <3020466737@qq.com>
2023-11-23 16:13:35 +0800

    测试maven

---
+++ src/main/java/com/molen/test.java
@@ -0,0 +1,12 @@
+package com.molen;
+
+/**
+ * @Author: molen
+ * @Date: 15:58 2023/11/20
+ * @Description:
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("maven");
+    }
+}
