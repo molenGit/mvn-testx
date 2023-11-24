dc510effaa49a6032e0dab75de5bf0a3d0a462cb
molen <3020466737@qq.com>
2023-11-24 14:42:45 +0800

    新分支

--- src/main/java/com/molen/test.java
+++ src/main/java/com/molen/test.java
@@ -9,5 +9,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("maven");
         System.out.println("git");
+        System.out.println("branch1");
     }
 }
